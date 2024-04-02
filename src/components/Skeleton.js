9a8b34cb934e51c884c66a87e6c4d49928ad9dbd
mlee0967 <mlee0967@gmail.com>
2024-04-02 08:43:57 -0700

    update skeleton

--- src/components/Skeleton.js
+++ src/components/Skeleton.js
@@ -1,11 +1,22 @@
-function Skeleton(){
-  const line = <div className="h-4 bg-gray-200 rounded-full dark:bg-gray-700 mt-2 mb-2"></div>;
+function SkeletonBar({ additionalClasses }) {
+  const baseClasses = 'h-4 bg-gray-200 rounded-full dark:bg-gray-700 mt-2 mb-2';
+  const classNames = additionalClasses ? `${baseClasses} ${additionalClasses}` : baseClasses;
+
   return (
-    <div className="w-32 animate-pulse items-center justify-center">
-      {line}
-      {line}
+    <div className={`${classNames}`} />
+  );
+}
+
+function Skeleton() {
+  return (
+    <div className='w-32 animate-pulse items-center justify-center'>
+      <SkeletonBar/>
+      <div className='flex'>
+        <SkeletonBar additionalClasses='w-6 mr-2'/>
+        <SkeletonBar additionalClasses='w-24' />
+      </div>
     </div>
-  )
+  );
 }
 
 export default Skeleton;
